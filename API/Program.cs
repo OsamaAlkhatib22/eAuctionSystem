d567739e99b459a06f1514f3e68ead2b59e8064c
SuhibAlsaggar <sohib.saggar90@gmail.com>
2023-03-19 11:30:38 +0300

    Refactored Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,22 +1,15 @@
 using Persistence;
 using Microsoft.EntityFrameworkCore;
 using Application;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(
-    opt => opt.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
-builder.Services.AddMediatR(
-    cfg => cfg.RegisterServicesFromAssembly(typeof(MediatREntrypoint).Assembly)
-);
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddCorsService(builder.Configuration, builder.Environment);
 
 var app = builder.Build();
 
@@ -27,22 +20,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
+
 app.UseAuthorization();
 
 app.MapControllers();
-
-using var scope = app.Services.CreateScope();
-var services = scope.ServiceProvider;
-try
-{
-    var context = services.GetRequiredService<DataContext>();
-    await context.Database.MigrateAsync();
-    //await Seed.SeedData(context);
-}
-catch (Exception ex)
-{
-    var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex, "An error occured during migration");
-}
 
 app.Run();
