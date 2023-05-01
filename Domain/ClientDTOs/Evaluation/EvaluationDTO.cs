37bd468462b909e9970afae1c2e24e85bba60381
Kareemaq02 <kareemabuqaoud@hotmail.com>
2023-05-02 00:19:25 +0300

    EvaluationDTO- Created one DTO that will be used for all evaluation options

---
+++ Domain/ClientDTOs/Evaluation/EvaluationDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Domain.ClientDTOs.Evaluation
+{
+    public class EvaluationDTO
+    {
+        [JsonIgnore]
+        public string strUserName { get; set; }
+        public string strComment { get; set; }
+        public decimal decRating { get; set; }
+    }
+}
+
+
+
