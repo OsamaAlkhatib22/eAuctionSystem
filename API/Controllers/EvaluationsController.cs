c4ccb81d9ee5e2a3da2b1836cf7e45e90bde322c
Kareemaq02 <kareemabuqaoud@hotmail.com>
2023-05-02 00:53:28 +0300

    EvaluationsController- Added FailTask Request

--- API/Controllers/EvaluationsController.cs
+++ API/Controllers/EvaluationsController.cs
@@ -7,7 +7,7 @@ namespace API.Controllers
 {
     public class EvaluationsController : BaseApiController
     {
-        [HttpPost("{id}")] // .../api/evaluations/...
+        [HttpPost("complete/{id}")] // .../api/evaluations/complete/...
         public async Task<IActionResult> CompleteTask(EvaluationDTO completedDTO, int id )// Evaluate selected task as completed
         {
             string authHeader = Request.Headers["Authorization"];
@@ -16,6 +16,16 @@ namespace API.Controllers
 
             completedDTO.strUserName = jwtToken.Claims.First(c => c.Type == "username").Value;
             return HandleResult(await Mediator.Send(new CompleteTaskCommand(completedDTO, id)));
+        }
+        [HttpPost("fail/{id}")] // .../api/evaluations/fail/...
+        public async Task<IActionResult> FailTask(EvaluationDTO failedDTO, int id)// Evaluate selected task as failed
+        {
+            string authHeader = Request.Headers["Authorization"];
+            JwtSecurityTokenHandler tokenHandler = new();
+            JwtSecurityToken jwtToken = tokenHandler.ReadJwtToken(authHeader[7..]);
+
+            failedDTO.strUserName = jwtToken.Claims.First(c => c.Type == "username").Value;
+            return HandleResult(await Mediator.Send(new FailTaskCommand(failedDTO, id)));
         }
 
 
