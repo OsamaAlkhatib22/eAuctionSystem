973e88ed9a15acddf090eb17d8f16023666b356e
Kareemaq02 <kareemabuqaoud@hotmail.com>
2023-05-02 01:25:57 +0300

    ComplaintTypeDTO- Edited the previous ComplaintTypeDTO so that it suits the insert task type request parameters

--- Domain/ClientDTOs/Complaint/ComplaintTypeDTO.cs
+++ Domain/ClientDTOs/Complaint/ComplaintTypeDTO.cs
@@ -1,8 +1,14 @@
-﻿namespace Domain.ClientDTOs.Complaint
+﻿using System.Text.Json.Serialization;
+
+namespace Domain.ClientDTOs.Complaint
 {
     public class ComplaintTypeDTO
     {
-        public int intId { get; set; }
+        [JsonIgnore]
+        public string strUserName { get; set; }
+        public decimal decGrade { get; set; }
+        public int intPrivacyId { get; set; }
+        public int intDepartmentId { get; set; }
         public string strNameAr { get; set; }
         public string strNameEn { get; set; }
     }
