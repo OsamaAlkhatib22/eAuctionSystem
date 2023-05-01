bb5adec874e99eaaaa001454b7ed0f46c18c0ef1
Kareemaq02 <kareemabuqaoud@hotmail.com>
2023-05-02 00:17:29 +0300

    EvaluationsController- Added CompleteTask request

---
+++ API/Controllers/EvaluationsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Application;
+using System.IdentityModel.Tokens.Jwt;
+using Domain.ClientDTOs.Evaluation;
+
+namespace API.Controllers
+{
+    public class EvaluationsController : BaseApiController
+    {
+        [HttpPost("{id}")] // .../api/evaluations/...
+        public async Task<IActionResult> CompleteTask(EvaluationDTO completedDTO, int id )// Evaluate selected task as completed
+        {
+            string authHeader = Request.Headers["Authorization"];
+            JwtSecurityTokenHandler tokenHandler = new();
+            JwtSecurityToken jwtToken = tokenHandler.ReadJwtToken(authHeader[7..]);
+
+            completedDTO.strUserName = jwtToken.Claims.First(c => c.Type == "username").Value;
+            return HandleResult(await Mediator.Send(new CompleteTaskCommand(completedDTO, id)));
+        }
+
+
+    }
+}
