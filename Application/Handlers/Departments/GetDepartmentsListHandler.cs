e75f1fdfeaf5ba94189f7c14402b830a64471a2e
Kareemaq02 <kareemabuqaoud@hotmail.com>
2023-05-16 14:58:22 +0300

    DepartmentsController- Added GetDepartmentsList

---
+++ Application/Handlers/Departments/GetDepartmentsListHandler.cs
@@ -0,0 +1,39 @@
+﻿using Application.Core;
+using Application.Queries.Departments;
+using Domain.ClientDTOs.Department;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Handlers.Departments
+{
+    public class GetDepartmentsListHandler
+        : IRequestHandler<GetDepartmentsListQuery, Result<List<DepartmentDTO>>>
+    {
+        private readonly DataContext _context;
+
+        public GetDepartmentsListHandler(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Result<List<DepartmentDTO>>> Handle(
+            GetDepartmentsListQuery request,
+            CancellationToken cancellationToken
+        )
+        {
+            List<DepartmentDTO> result = await _context.Departments
+                .Select(
+                    q =>
+                        new DepartmentDTO
+                        {
+                            strNameAr = q.strNameAr,
+                            strNameEn = q.strNameEn,
+                        }
+                )
+                .ToListAsync();
+
+            return Result<List<DepartmentDTO>>.Success(result);
+        }
+    }
+}
