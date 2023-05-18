4127d3e154d45daca76030419ae24d06050908b5
ibramatar07 <otakuman789@gmail.com>
2023-05-18 19:46:43 +0300

    view proffessions- datagrid to show proffessions

---
+++ client/src/Views/Proffessions/Views/AdminProffessionView.jsx
@@ -0,0 +1,25 @@
+import { Typography, SwipeableDrawer } from "@mui/material";
+import { useEffect, useState } from "react";
+import ProffessionDataGrid from "../Components/ProffessionDataGrid";
+import GetProffession from "../Service/GetProffession";
+
+function AdminProffession() {
+  const [proffession, setProffession] = useState([]);
+
+  useEffect(() => {
+    const setProffessions = async () => {
+      const response = await GetProffession();
+      setProffession(response.data);
+    };
+    setProffessions();
+  }, []);
+
+  return (
+    <div>
+      <Typography variant="h1">View Proffessions</Typography>
+      <ProffessionDataGrid data={proffession} />
+    </div>
+  );
+}
+
+export default AdminProffession;
